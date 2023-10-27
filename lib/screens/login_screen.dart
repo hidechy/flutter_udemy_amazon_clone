e6ad5bd4c1fc3b560cbee0e2628f991374ff6e48
toyohide <hide.toyoda@gmail.com>
2023-10-28 08:25:22 +0900

    commit 20231028

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/custom_text_field.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -8,10 +10,62 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  ///
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('LoginScreen'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Image.asset('assets/images/welcome.png'),
+          ),
+          const SizedBox(height: 20),
+          Form(
+            key: formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  textEditingController: emailTextEditingController,
+                  iconData: Icons.email,
+                  hintText: 'Email',
+                  isObsecre: false,
+                  enabled: true,
+                ),
+                const SizedBox(height: 5),
+                CustomTextField(
+                  textEditingController: passwordTextEditingController,
+                  iconData: Icons.lock,
+                  hintText: 'password',
+                  isObsecre: false,
+                  enabled: true,
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.pinkAccent,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            ),
+            onPressed: () {},
+            child: const Text(
+              'Sign In',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
