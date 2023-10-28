82f6a8953e695a7bf99a3713a4d49dadff614606
toyohide <hide.toyoda@gmail.com>
2023-10-28 13:59:32 +0900

    commit 20231028 item display

---
+++ lib/components/items_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../models/items.dart';
+
+// ignore: must_be_immutable
+class ItemsCard extends StatefulWidget {
+  ItemsCard({super.key, this.model, this.context});
+
+  Items? model;
+  BuildContext? context;
+
+  @override
+  State<ItemsCard> createState() => _ItemsCardState();
+}
+
+class _ItemsCardState extends State<ItemsCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shadowColor: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: SizedBox(
+          height: 270,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              const SizedBox(height: 2),
+              Text(
+                widget.model!.itemTitle.toString(),
+                style: const TextStyle(
+                  color: Colors.deepPurple,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  letterSpacing: 3,
+                ),
+              ),
+              const SizedBox(height: 2),
+              Image.network(
+                widget.model!.thumbnailUrl.toString(),
+                height: 220,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 4),
+              Text(
+                widget.model!.itemInfo.toString(),
+                style: const TextStyle(color: Colors.grey, fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -1,17 +1,17 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:test_udemy_amazon_clone2/screens/items_upload_screen.dart';
-import 'package:test_udemy_amazon_clone2/styles/styles.dart';
-import '../components/text_delegate_header.dart';
+
+import '../components/items_card.dart';
+import '../components/shared_prefs.dart';
+import '../extensions/extensions.dart';
 import '../models/brands.dart';
+import '../models/items.dart';
+import '../styles/styles.dart';
+import 'items_upload_screen.dart';
 
 // ignore: must_be_immutable
 class ItemsScreen extends StatefulWidget {
-  ItemsScreen({
-    super.key,
-    this.model,
-  });
+  ItemsScreen({super.key, this.model});
 
   Brands? model;
 
@@ -50,13 +50,44 @@ class _ItemsScreenState extends State<ItemsScreen> {
           ),
         ],
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: TextDelegateHeader(title: 'My ' + widget.model!.brandTitle.toString() + "'s Items"),
-          ),
-        ],
+      body: SizedBox(
+        height: context.screenSize.height * 0.8,
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('sellers')
+              .doc(sharedPreferences!.getString('uid'))
+              .collection('brands')
+              .doc(widget.model!.brandID)
+              .collection('items')
+              .orderBy('publishedDate', descending: true)
+              .snapshots(),
+          // ignore: strict_raw_type
+          builder: (context, AsyncSnapshot dataSnapshot) {
+            if (dataSnapshot.hasData) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final itemsModel = Items.fromJson(
+                    // ignore: avoid_dynamic_calls
+                    dataSnapshot.data.docs[index].data() as Map<String, dynamic>,
+                  );
+
+                  return ItemsCard(
+                    model: itemsModel,
+                    context: context,
+                  );
+                },
+                // ignore: avoid_dynamic_calls
+                itemCount: dataSnapshot.data.docs.length,
+              );
+            } else {
+              return const Center(
+                child: Text(
+                  'No items exists',
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
