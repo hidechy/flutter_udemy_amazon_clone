5c97f0982912b504e83e2bc3e941d71fb52fe09d
toyohide <hide.toyoda@gmail.com>
2023-10-28 10:01:31 +0900

    commit 20231028 create account

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple, brightness: Brightness.dark),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: const AuthScreen(),
       debugShowCheckedModeBanner: false,
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart' as fire_storage;
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../components/custom_text_field.dart';
-
+import '../components/loading_dialog.dart';
+import '../components/shared_prefs.dart';
 import '../extensions/extensions.dart';
+import 'splash_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -19,6 +29,12 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
+  XFile? imgXFile;
+
+  final ImagePicker imagePicker = ImagePicker();
+
+  String downloadUrlImage = '';
+
   ///
   @override
   Widget build(BuildContext context) {
@@ -31,15 +47,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             child: Column(
               children: [
                 GestureDetector(
-                  onTap: () {},
+                  onTap: getImageFromGallery,
                   child: CircleAvatar(
-                    radius: context.screenSize.width * 0.2,
+                    radius: context.screenSize.width * 0.20,
                     backgroundColor: Colors.white,
-                    child: Icon(
-                      Icons.add_photo_alternate,
-                      color: Colors.grey,
-                      size: context.screenSize.width * 0.2,
-                    ),
+                    backgroundImage: (imgXFile == null) ? null : FileImage(File(imgXFile!.path)),
+                    child: imgXFile == null
+                        ? Icon(
+                            Icons.add_photo_alternate,
+                            color: Colors.grey,
+                            size: MediaQuery.of(context).size.width * 0.2,
+                          )
+                        : null,
                   ),
                 ),
                 const SizedBox(height: 20),
@@ -83,7 +102,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               backgroundColor: Colors.pinkAccent,
               padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
             ),
-            onPressed: () {},
+            onPressed: formValidation,
             child: const Text(
               'Sign Up',
               style: TextStyle(
@@ -92,8 +111,108 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               ),
             ),
           ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {
+              nameTextEditingController.text = 'toyohide';
+              emailTextEditingController.text = 'hide.toyoda@gmail.com';
+              passwordTextEditingController.text = 'hidechy4819';
+              confirmPasswordTextEditingController.text = 'hidechy4819';
+            },
+            child: const Text('dummy'),
+          ),
         ],
       ),
     );
+  }
+
+  ///
+  Future<void> getImageFromGallery() async {
+    imgXFile = await imagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      // ignore: unnecessary_statements
+      imgXFile;
+    });
+  }
+
+  ///
+  Future<void> formValidation() async {
+    if (imgXFile == null) {
+      await Fluttertoast.showToast(msg: 'Please select an image.');
+    } else {
+      if (passwordTextEditingController.text == confirmPasswordTextEditingController.text) {
+        if (nameTextEditingController.text.isNotEmpty &&
+            emailTextEditingController.text.isNotEmpty &&
+            passwordTextEditingController.text.isNotEmpty &&
+            confirmPasswordTextEditingController.text.isNotEmpty) {
+          await showDialog(
+            context: context,
+            builder: (context) => const LoadingDialog(message: 'Registering your account'),
+          );
+
+          final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+          final storageRef = fire_storage.FirebaseStorage.instance.ref().child('sellersImages').child(fileName);
+          final uploadImageTask = storageRef.putFile(File(imgXFile!.path));
+          final taskSnapshot = await uploadImageTask.whenComplete(() {});
+          await taskSnapshot.ref.getDownloadURL().then((urlImage) => downloadUrlImage = urlImage);
+
+          await saveInformationToDatabase();
+        } else {
+          Navigator.pop(context);
+
+          await Fluttertoast.showToast(msg: 'text field empty.');
+        }
+      } else {
+        await Fluttertoast.showToast(msg: 'Password not match.');
+      }
+    }
+  }
+
+  ///
+  Future<void> saveInformationToDatabase() async {
+    User? currentUser;
+
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+          email: emailTextEditingController.text.trim(),
+          password: passwordTextEditingController.text.trim(),
+        )
+        .then((auth) => currentUser = auth.user)
+        .catchError(
+      // ignore: inference_failure_on_untyped_parameter, body_might_complete_normally_catch_error
+      (errorMessage) {
+        Navigator.pop(context);
+
+        Fluttertoast.showToast(msg: 'Error Occurred: \n $errorMessage');
+      },
+    );
+
+    if (currentUser != null) {
+      await saveInfoToFirestoreAndLocally(currentUser!);
+    }
+  }
+
+  ///
+  Future<void> saveInfoToFirestoreAndLocally(User currentUser) async {
+    await FirebaseFirestore.instance.collection('sellers').doc(currentUser.uid).set(
+      {
+        'uid': currentUser.uid,
+        'email': currentUser.email,
+        'name': nameTextEditingController.text.trim(),
+        'photoUrl': downloadUrlImage,
+        'status': 'approved',
+        'earnings': 0.0,
+      },
+    );
+
+    sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences!.setString('uid', currentUser.uid);
+    await sharedPreferences!.setString('email', currentUser.email!);
+    await sharedPreferences!.setString('name', nameTextEditingController.text.trim());
+    await sharedPreferences!.setString('photoUrl', downloadUrlImage);
+
+    // ignore: use_build_context_synchronously
+    await Navigator.push(context, MaterialPageRoute(builder: (c) => const MySplashScreen()));
   }
 }
